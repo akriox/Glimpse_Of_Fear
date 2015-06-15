f8a44912c053da5fc9afabbd3390e98850db7e74
akriox <akriox666@gmail.com>
2015-06-15 18:29:18 +0200

    Clean up

--- Assets/Scripts/Utility/Framerate.cs
+++ Assets/Scripts/Utility/Framerate.cs
@@ -6,7 +6,6 @@ public class Framerate : MonoBehaviour
 {
 	private Text framerate;
 	private float deltaTime = 0.0f;
-	private float ms = 0.0f;
 	private float fps = 0.0f;
 	
 	public void Start(){
@@ -16,8 +15,7 @@ public class Framerate : MonoBehaviour
 	public void Update()
 	{
 		deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
-		ms = deltaTime * 1000.0f;
 		fps = 1.0f / deltaTime;
-		framerate.text = string.Format("{0:0.0} ms ({1:0.} fps)", ms, fps);
+		framerate.text = (int)fps + "FPS";
 	}
 }
