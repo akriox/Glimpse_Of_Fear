cc014aaa67cf062a5f2c67dc24e2fcea9a739765
blonduel <blondjeremie31@gmail.com>
2015-06-16 16:59:16 +0200

    darkRoom
    DarkRoom With guide and collider room

---
+++ Assets/Scripts/DarkRoom/FollowPath.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+[RequireComponent(typeof(GazeAwareComponent))]
+public class FollowPath : MonoBehaviour {
+	public static FollowPath Instance {get; private set;}
+
+	public Transform pathToFollow;
+	[SerializeField][Range(2F, 10.0F)] public float speed;
+	[SerializeField][Range(2F, 5.0F)] public float DistanceMinForMove;
+
+	
+	private List<Transform> listPaths = new List<Transform>();
+	private int index = 1;
+	private Transform currentTarget;
+	private GameObject player;
+	private GazeAwareComponent _gazeAwareComponent;
+	private AudioSource song;
+
+	public void Awake(){
+		Instance = this;
+	}
+
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player");
+		_gazeAwareComponent = GetComponent<GazeAwareComponent>();
+		song = GetComponent<AudioSource> ();
+		if(pathToFollow == null){
+			Debug.LogError("Un GameObject 'Path' doit etre renseigné dans le script 'FollowPath.cs'.");
+		} 
+		else {
+			GetPaths();
+			index = 1;
+			if(listPaths.Count > 0) GetNewPosition();
+		}
+	}
+
+	void Update () {
+		if (_gazeAwareComponent.HasGaze) {
+			if (!song.isPlaying)
+				song.Play ();
+		} else {
+			song.Stop ();
+		}
+		Walk();
+	}
+
+	void GetPaths(){
+		foreach(Transform temp in pathToFollow){
+				listPaths.Add(temp);
+		}
+	}
+
+	void GetNewPosition(){
+		currentTarget = listPaths.Single(p => p.name == "Path" + index);
+		index = (index < listPaths.Count) ? index +1 : 1;
+	}
+
+
+	void Walk(){
+		if(currentTarget != null){
+			transform.position = Vector3.MoveTowards(transform.position, currentTarget.position, Time.deltaTime * speed);
+
+			if(CheckDistance() <= 0.5f && CheckPlayerDistance()<=DistanceMinForMove){
+				GetNewPosition();
+			}
+		}
+	}
+	float CheckPlayerDistance(){
+		return Vector3.Distance(transform.position, player.transform.position);
+	}
+
+	float CheckDistance(){
+		return Vector3.Distance(transform.position, currentTarget.position);
+	}
+
+	public void resetFirstRoom(){
+		print ("coucou");
+		index = 1;
+		currentTarget = listPaths [index-1];
+		transform.position = new Vector3 (currentTarget.transform.position.x, currentTarget.transform.position.y, currentTarget.transform.position.z);
+	}
+	public void resetSecondRoom(){
+		index = 6;
+		currentTarget = listPaths [index-1];
+		transform.position = new Vector3 (currentTarget.transform.position.x, currentTarget.transform.position.y, currentTarget.transform.position.z);
+	}
+	public void resetThirdRoom(){
+		index = 13;
+		currentTarget = listPaths [index-1];
+		transform.position = new Vector3 (currentTarget.transform.position.x, currentTarget.transform.position.y, currentTarget.transform.position.z);
+	}
+	public void resetFourthRoom(){
+		index = 20;
+		currentTarget = listPaths [index-1];
+		transform.position = new Vector3 (currentTarget.transform.position.x, currentTarget.transform.position.y, currentTarget.transform.position.z);
+	}
+}
---
+++ Assets/Scripts/DarkRoom/RoomTrigger.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoomTrigger: MonoBehaviour {
+
+
+	[SerializeField][Range(1, 4)]public int numRoom = 1;
+	
+	public void OnCollisionStay(Collision other) {
+
+		if (other.gameObject.name == "Player") {
+			Debug.Log ("coucou");
+			switch (numRoom) {
+			case 1:
+				FollowPath.Instance.resetFirstRoom ();
+				break;
+			case 2:
+				FollowPath.Instance.resetSecondRoom ();
+				break;
+			case 3:
+				FollowPath.Instance.resetThirdRoom ();
+				break;
+			case 4:
+				FollowPath.Instance.resetFourthRoom ();
+				break;
+			}
+		}
+	}
+}
