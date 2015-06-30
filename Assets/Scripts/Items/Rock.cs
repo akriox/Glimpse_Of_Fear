538177bdd07f582dfef9dc58ea2cfd88c262f484
akriox <akriox666@gmail.com>
2015-06-30 10:25:58 +0200

    CameraShake+ThrowObject

---
+++ Assets/Scripts/Items/Rock.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rock : Collectible {
+
+	private GameObject player;
+	private Rigidbody rb;
+
+	public void Start(){
+		player = GameObject.FindGameObjectWithTag("Player");
+		rb = GetComponent<Rigidbody>();
+	}
+
+	public void Update(){
+		if(pickUp){
+			rb.isKinematic = true;
+			this.gameObject.transform.parent = player.transform;
+			ThrowObject.setObjectToThrow(this.gameObject);
+		}
+
+		if(rb.isKinematic == false){
+			this.gameObject.transform.parent = null;
+		}
+	}
+}
---
+++ Assets/Scripts/Player/ThrowObject.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using XInputDotNetPure;
+
+[RequireComponent (typeof(GazePointDataComponent))]
+public class ThrowObject : MonoBehaviour {
+	
+	private GazePointDataComponent _gazePointDataComponent;
+	private string GlowStickPrefab = "Prefabs/Items/GlowStick";
+	private static GameObject objectToThrow;
+	private float force = 2000.0f;
+	private Vector3 trajectory;
+	private bool throwing;
+	
+	public void Start(){
+		_gazePointDataComponent = GetComponent<GazePointDataComponent>();
+	}
+	
+	public void Update(){
+		
+		var gazePoint = _gazePointDataComponent.LastGazePoint;
+		
+		if(gazePoint.IsValid && gazePoint.IsWithinScreenBounds){
+
+			if(Input.GetButtonDown("Throw")){
+				if(Inventory.Instance.glowStickStash > 0 && objectToThrow == null){
+					objectToThrow = Instantiate(Resources.Load (GlowStickPrefab), transform.position, Quaternion.identity) as GameObject;
+					objectToThrow.name = "Glowstick";
+					Inventory.Instance.removeGlowStick(1);
+				}
+				if(objectToThrow != null){
+					throwing = true;
+				
+					Ray ray = Camera.main.ScreenPointToRay(gazePoint.Screen);
+					RaycastHit hit;
+					
+					if (Physics.Raycast (ray, out hit)){
+						trajectory = hit.point - objectToThrow.transform.position;
+						trajectory.y += 3.0f;
+					}
+				}
+			}
+		}
+	}
+	
+	public void FixedUpdate(){
+		if(objectToThrow && throwing){
+			StartCoroutine(Vibration());
+			Rigidbody rb = objectToThrow.GetComponent<Rigidbody>();
+			if(rb.isKinematic) rb.isKinematic = false;
+			rb.AddForce(trajectory * force * Time.fixedDeltaTime);
+			objectToThrow = null;
+			throwing = false;
+		}
+	}
+
+	public static void setObjectToThrow(GameObject go){
+		objectToThrow = go;
+	}
+	
+	private IEnumerator Vibration(){
+		GamePad.SetVibration(0, 0.5f, 0.5f);
+		yield return new WaitForSeconds(0.5f);
+		GamePad.SetVibration(0, 0.0f, 0.0f);
+	}
+}
