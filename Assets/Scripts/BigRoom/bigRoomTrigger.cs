deb30967df51f0b2ac9046d22a07b20f0b1d7911
blonduel <blondjeremie31@gmail.com>
2015-07-02 19:02:31 +0200

    triggerBigRoom
    fait apparaitre le reaper a certain passage de la grande salle avec
    stalactite

---
+++ Assets/Scripts/BigRoom/bigRoomTrigger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace UnityStandardAssets.ImageEffects
+{
+	public class bigRoomTrigger: MonoBehaviour {
+
+		[SerializeField]private GameObject Path;
+		void Start(){
+			Path.SetActive(false);
+		}
+		public void OnTriggerEnter(Collider other) {
+			if (other.gameObject.tag == "Player") {
+				Path.SetActive(true);
+			}
+		}
+		public void OnTriggerExit(Collider other) {
+			if (other.gameObject.tag == "Player") {
+				Path.SetActive(false);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/BigRoom/lookPathReaper.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(GazeAwareComponent))]
+public class lookPathReaper : MonoBehaviour {
+	
+	private GazeAwareComponent _gazeAwareComponent;
+	[SerializeField] private Transform positionStart;
+	[SerializeField] private Transform PositionEnd;
+	[SerializeField]private GameObject OtherTrigger;
+
+	private bool alreadyCall = false;
+
+
+	public void Start(){
+		//get component for eye tracker
+		_gazeAwareComponent = GetComponent<GazeAwareComponent> ();
+	}
+	void Update () {
+		if (_gazeAwareComponent.HasGaze && !alreadyCall) {
+			moveReaperBigRoom.Instance.setPosition (positionStart.position, PositionEnd.position);
+			alreadyCall = true;
+			if(OtherTrigger != null)
+				OtherTrigger.SetActive(false);
+		}
+	}
+}
