282d80353103d27cb4a947e6ad985bb46c008f15
blonduel <blondjeremie31@gmail.com>
2015-08-10 13:50:26 +0200

    cristal spirit finish
    des cristaux s'allument au passage du spirit

--- Assets/Scripts/SpiritCave2/CristalSpirit.cs
+++ Assets/Scripts/SpiritCave2/CristalSpirit.cs
@@ -3,13 +3,41 @@ using System.Collections;
 
 public class CristalSpirit : MonoBehaviour {
 
+	private Material activeMat;
+	private Material inactiveMat;
+	private GameObject targetSpirit;
+	private float distActivationCrystal = 5f;
+
 	// Use this for initialization
 	void Start () {
-	
+		inactiveMat = (Material)Resources.Load("Materials/Caves_objects/Crystal", typeof(Material));
+		activeMat = (Material)Resources.Load("Materials/Caves_objects/CrystalSpirit", typeof(Material));
+		gazeEffects (inactiveMat);
+		targetSpirit = GameObject.FindGameObjectWithTag("Specter");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (closeToTheTarget (distActivationCrystal, targetSpirit)) {
+			gazeEffects(activeMat);
+		} 
+		else {
+			gazeEffects(inactiveMat);
+		}
+	}
+
+	/** Change material color and enable/disable Halo */
+	private void gazeEffects(Material mat) {
+		GetComponent<Renderer>().material = mat;
+	}
+
+	//return true if the spirit is close to the crystal 
+	private bool closeToTheTarget(float dist, GameObject target){
+		if (Vector3.Distance(transform.position, target.transform.position) < dist) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 }
