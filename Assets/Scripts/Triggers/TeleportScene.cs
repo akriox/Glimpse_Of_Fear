6840990f2a5d016e3a0854bc26e3c79dc095e299
akriox <akriox666@gmail.com>
2015-10-11 22:36:11 +0200

    Teleport Sound

--- Assets/Scripts/Triggers/TeleportScene.cs
+++ Assets/Scripts/Triggers/TeleportScene.cs
@@ -7,15 +7,19 @@ public class TeleportScene : MonoBehaviour {
     public int sceneIndex;
     private bool _playerOpenEyes;
     private UserPresenceComponent _userPresenceComponent;
+	private AudioClip audioClip;
 
     public void Start(){
         _userPresenceComponent = GetComponent<UserPresenceComponent>();
+		audioClip = (AudioClip) Resources.Load("Audio/Teleport", typeof(AudioClip));
     }
 
     public void OnTriggerStay(Collider other){
         if (other.gameObject.tag == "Player"){
-			if(_userPresenceComponent.GazeTracking == EyeXGazeTracking.GazeNotTracked)
+			if(_userPresenceComponent.GazeTracking == EyeXGazeTracking.GazeNotTracked){
+				EventSound.playClip(audioClip);
             	LoadingScreen.Instance.fadeToBlack(sceneIndex);
+			}
         }
     }
 }
