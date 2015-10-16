86adee5a93ae065c594dde5217b9f65ce27dda44
blonduel <blondjeremie31@gmail.com>
2015-10-16 13:09:40 +0200

    final update

--- Assets/Scripts/Triggers/TeleportScene.cs
+++ Assets/Scripts/Triggers/TeleportScene.cs
@@ -8,17 +8,24 @@ public class TeleportScene : MonoBehaviour {
     private bool _playerOpenEyes;
     private UserPresenceComponent _userPresenceComponent;
 	private AudioClip audioClip;
+	private bool alreadyCall;
 
     public void Start(){
         _userPresenceComponent = GetComponent<UserPresenceComponent>();
 		audioClip = (AudioClip) Resources.Load("Audio/Teleport", typeof(AudioClip));
+		alreadyCall = false;
     }
 
     public void OnTriggerStay(Collider other){
         if (other.gameObject.tag == "Player"){
-			if(_userPresenceComponent.GazeTracking == EyeXGazeTracking.GazeNotTracked){
+			if(_userPresenceComponent.GazeTracking == EyeXGazeTracking.GazeNotTracked && !alreadyCall){
 				EventSound.playClip(audioClip);
-            	LoadingScreen.Instance.fadeToBlack(sceneIndex);
+				alreadyCall = true;
+				if(sceneIndex != 0)
+            		LoadingScreen.Instance.fadeToBlack(sceneIndex);
+				else{
+					Application.LoadLevel (0);
+				}
 			}
         }
     }
